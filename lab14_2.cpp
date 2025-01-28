468bd14ac6b562733ac6de4bca3fa4a7de702e76
Teetad <teetadraseesit@gmail.com>
2025-01-28 13:15:59 +0700

    lab14_1 done

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -5,6 +5,7 @@ const int N = 5;
 
 void inputMatrix(double [][N]);
 
+
 void findLocalMax(const double [][N], bool [][N]);
 
 void showMatrix(const bool [][N]);
@@ -21,3 +22,37 @@ int main(){
 }
 
 //Do not modify source code above this line 
+void  inputMatrix(double inMatrix[N][N]){
+	double storage;
+	for (int i = 0; i < N; i++){
+		cout << "Row "<< i + 1<< ": ";
+		for (int j = 0; j < N; j++){
+			cin >> storage;
+			inMatrix[i][j] = storage;
+		}
+	}
+}
+
+void findLocalMax(const double inMatrix[][N], bool ans[N][N]){
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j < N; j++){
+			if (i == 0 || i == N - 1 || j == 0 || j == N - 1){
+				ans[i][j] = 0;
+			} 
+			else if (inMatrix[i][j] >= inMatrix[i-1][j] && inMatrix[i][j] >= inMatrix[i+1][j] && inMatrix[i][j] >= inMatrix[i][j-1] && inMatrix[i][j] >= inMatrix[i][j+1]){
+				ans[i][j] = 1;
+			}else{
+				ans[i][j] = 0;
+			}
+		}
+	}
+}
+
+void showMatrix(const bool show[][N]){
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j < N; j++){
+			cout << show[i][j] << ' ';
+		}
+		cout << "\n";
+	}
+}
